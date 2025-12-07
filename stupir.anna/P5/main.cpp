e3ef2731238f0ac49e2572f4e65f276fa90613a8
anyable <stupiranna@mail.ru>
2025-12-07 17:46:16 +0300

    interface Shape

--- stupir.anna/P5/main.cpp
+++ stupir.anna/P5/main.cpp
@@ -1,4 +1,29 @@
-int main()
+namespace stupir
 {
-
+  struct point_t;
+  struct rectangle_t;
+  struct Shape;
+  struct Rectangle;
 }
+struct point_t
+{
+  double x;
+  double y;
+};
+struct rectangle_t
+{
+  double width;
+  double height;
+  point_t pos;
+};
+struct Shape
+{
+  virtual double getArea() const = 0;
+  virtual rectangle_t getFrameRect() const = 0;
+  virtual void move(point_t);
+  virtual void move(double x, double y);
+  virtual void scale(double k) = 0;
+  virtual ~Shape();
+};
+
+
