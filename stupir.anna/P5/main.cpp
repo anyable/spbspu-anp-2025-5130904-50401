44b159737b95a79420179c4422dd4b475408b13c
anyable <stupiranna@mail.ru>
2025-12-09 22:04:35 +0300

    realized Square and Xquare

--- stupir.anna/P5/main.cpp
+++ stupir.anna/P5/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 namespace stupir
 {
   struct point_t;
@@ -6,49 +7,52 @@ namespace stupir
   struct seg_t;
   struct Shape;
   struct Rectangle;
+  struct Square;
+  struct Xquare;
 }
+
 struct stupir::point_t
 {
   double x;
   double y;
 };
+
 struct stupir::rectangle_t
 {
   double width;
   double height;
   point_t pos;
 };
+
 struct stupir::Shape
 {
   virtual double getArea() const = 0;
   virtual rectangle_t getFrameRect() const = 0;
-  virtual void move(point_t p) = 0;
-  virtual void move(double x, double y) = 0;
-  virtual void scale(double k) = 0;
+  virtual void move(point_t) = 0;
+  virtual void move(double , double) = 0;
+  virtual void scale(double) = 0;
   virtual ~Shape() = default;
 };
-struct stupir::seg_t
-{
-  point_t a, b;
-};
+
+
 struct stupir::Rectangle : Shape
 {
-  Rectangle(double weight, double hight, point_t center);
+  Rectangle(point_t center, double weight, double hight);
   double getArea() const override;
   rectangle_t getFrameRect() const override;
   void move(point_t) override;
   void move(double x, double y) override;
   void scale(double k) override;
-  private:
+  protected:
     point_t c_;
     double w_;
     double h_;
 };
+
 void stupir::Rectangle::scale(double k)
 {
   h_ *= k, w_ *= k;
 }
-
 
 void stupir::Rectangle::move(double x, double y)
   {
@@ -56,14 +60,13 @@ void stupir::Rectangle::move(double x, double y)
     c_.y += y;
   }
 
-
 void stupir::Rectangle::move(point_t p)
 {
   c_ = p;
 }
 
-stupir::Rectangle::Rectangle(double weight, double hight, point_t center):
-  c_(center), w_(weight), h_(hight)
+stupir::Rectangle::Rectangle(point_t center, double weight, double hight):
+  Shape(), c_(center), w_(weight), h_(hight)
   {}
 
 double stupir::Rectangle::getArea() const
@@ -76,17 +79,32 @@ stupir::rectangle_t stupir::Rectangle::getFrameRect() const
   return {w_, h_, c_};
 }
 
+struct stupir::Square : Rectangle
+{
+  Square(point_t , double);
+};
+
+stupir::Square::Square(point_t center, double len):
+  Rectangle(center, len, len)
+  {}
+
+struct stupir::Xquare : Square
+{
+  Xquare(point_t, double);
+  rectangle_t getFrameRect() const override;
+};
+
+stupir::Xquare::Xquare(point_t center, double len):
+  Square(center, len)
+  {}
+
+stupir::rectangle_t stupir::Xquare::getFrameRect() const
+{
+  double d = std::sqrt(w_ * w_ + h_ * h_);
+  return {d , d , c_};
+}
+
 int main()
 {
-  using namespace stupir;
-  stupir::Rectangle f1(2, 3, {2, 3});
-  double s1 = f1.getArea();
-  rectangle_t ss1 = f1.getFrameRect();
-
-  f1.scale(4);
-  double s2 = f1.getArea();
-  rectangle_t ss2 = f1.getFrameRect();
-
-  std::cout << s1 << ' ' << ss1.height << ' ' << ss1.width << '\n';
-  std::cout << s2 << ' ' << ss2.height << ' ' << ss2.width << '\n';
+ 
 }
