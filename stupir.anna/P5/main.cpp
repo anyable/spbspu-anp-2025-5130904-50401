5f5ffded0e79f51684df83adccff6bca865aae28
anyable <stupiranna@mail.ru>
2025-12-10 02:39:12 +0300

    demo program

--- stupir.anna/P5/main.cpp
+++ stupir.anna/P5/main.cpp
@@ -9,6 +9,9 @@ namespace stupir
   struct Rectangle;
   struct Square;
   struct Xquare;
+  void scalePoint(point_t, double, Shape &);
+  std::ostream & operator<<(std::ostream &, const rectangle_t &);
+  rectangle_t getFrameAllFigure(const rectangle_t &, const rectangle_t &, const rectangle_t &);
 }
 
 struct stupir::point_t
@@ -33,7 +36,6 @@ struct stupir::Shape
   virtual void scale(double) = 0;
   virtual ~Shape() = default;
 };
-
 
 struct stupir::Rectangle : Shape
 {
@@ -104,7 +106,108 @@ stupir::rectangle_t stupir::Xquare::getFrameRect() const
   return {d , d , c_};
 }
 
+void stupir::scalePoint(point_t p, double k, Shape & f)
+{
+  point_t mid = f.getFrameRect().pos;
+  point_t end = {mid.x + p.x, mid.y + p.y};
+  f.move(p.x, p.y);
+  f.scale(k);
+  point_t begin = {mid.x * 2 - end.x, mid.y * 2 - end.y};
+  f.move(begin.x, begin.y);
+}
+
+std::ostream & stupir::operator<<(std::ostream & out, const stupir::rectangle_t & rec)
+{
+  out << "hight = " << rec.height << ", weight = " << rec.width << ", pos = {" ;
+  out << rec.pos.x << ", " << rec.pos.y << "}\n";
+  return out;
+}
+
+stupir::rectangle_t stupir::getFrameAllFigure(const stupir::rectangle_t & f1, const stupir::rectangle_t & f2, const stupir::rectangle_t & f3)
+{
+  double maxX = std::max(std::max(f1.pos.x, f2.pos.x), f3.pos.x);
+  double minX = std::min(std::min(f1.pos.x, f2.pos.x), f3.pos.x);
+  double maxY = std::max(std::max(f1.pos.y, f2.pos.y), f3.pos.y);
+  double minY = std::min(std::min(f1.pos.y, f2.pos.y), f3.pos.y);
+  double newWidht = std::abs(maxX - minX);
+  double newHight = std::abs(maxY - minY);
+  stupir::point_t newPos = {maxY + maxX / 2, minX + minY / 2};
+  return {newWidht, newHight, newPos};
+}
+
 int main()
 {
- 
+  namespace stu = stupir;
+  double k = 0;
+  stu::point_t scalePoint{0, 0};
+  std::cin >> scalePoint.x >> scalePoint.y >> k;
+  if (std::cin.fail())
+  {
+    std::cerr << "\nNot correct data for scale\n";
+    return 1;
+  }
+  else if (k <= 0)
+  {
+    std::cerr << "\nNot correct coefficient for scale(negative)\n";
+    return 1;
+  }
+  int err = 0;
+  stu::Shape ** f = nullptr;
+  try
+  {
+    f = new stu::Shape * [3]{nullptr};
+    f[0] = new stu::Rectangle({0, 0}, 2, 3);
+    f[1] = new stu::Square({3, 5}, 12);
+    f[2] = new stu::Xquare({-1, 4}, 5);
+    double areaRec = f[0] -> getArea();
+    double areaSq = f[1] -> getArea();
+    double areaXq = f[2] -> getArea();
+    stu::rectangle_t recRec = f[0] -> getFrameRect();
+    stu::rectangle_t recSq = f[1] -> getFrameRect();
+    stu::rectangle_t recXq = f[2] -> getFrameRect();
+
+    std::cout << "Area rectangle = " << areaRec << '\n';
+    std::cout << "The bounding rectangle of rectangle - " << recRec;
+    std::cout << "Area square = " << areaSq << '\n';
+    std::cout << "The bounding rectangle of square - " << recSq;
+    std::cout << "Area xquare = " << areaXq << '\n';
+    std::cout << "The bounding rectangle of xquare - " << recXq;
+    std::cout << "Summa areas all figure = " << areaRec + areaSq + areaXq << '\n';
+    std::cout << "The bounding rectangle of all figure - ";
+    std::cout << stu::getFrameAllFigure(recRec, recSq, recXq);
+
+    f[0] -> move({2, -6});
+    f[1] -> scale(4.5);
+    stu::scalePoint({6, -2}, 3.1, *f[2]);
+    double areaRecCh = f[0] -> getArea();
+    double areaSqCh = f[1] -> getArea();
+    double areaXqCh = f[2] -> getArea();
+    stu::rectangle_t recRecCh = f[0] -> getFrameRect();
+    stu::rectangle_t recSqCh = f[1] -> getFrameRect();
+    stu::rectangle_t recXqCh = f[2] -> getFrameRect();
+
+    std::cout << "Area rectangle = " << areaRecCh << '\n';
+    std::cout << "The bounding rectangle of rectangle - " << recRecCh;
+    std::cout << "Area square = " << areaSqCh << '\n';
+    std::cout << "The bounding rectangle of square - " << recSqCh;
+    std::cout << "Area xquare = " << areaXqCh << '\n';
+    std::cout << "The bounding rectangle of xquare - " << recXqCh;
+    std::cout << "Summa areas all figure = " << areaRecCh + areaSqCh + areaXqCh << '\n';
+    std::cout << "The bounding rectangle of all figure - ";
+    std::cout << stu::getFrameAllFigure(recRecCh, recSqCh, recXqCh);
+  }
+  catch(...)
+  {
+    std::cerr << "Could not allocete memory\n";
+    err = 1;
+    if (!f)
+    {
+      return err;
+    }
+  }
+  delete f[0];
+  delete f[1];
+  delete f[2];
+  delete [] f;
+  return err;
 }
